12beb8ae5391b5d1559b2c85a0f8e0c6ba1e2b14
victorchu8 <victorchu1996@gmail.com>
2018-02-07 22:58:51 -0700

    Added background and started textfield implementation

---
+++ Window-Shopper/Window-Shopper/Views/CurrencyTxtField.swift
@@ -0,0 +1,27 @@
+//
+//  CurrencyTxtField.swift
+//  Window-Shopper
+//
+//  Created by VICTOR CHU on 2018-02-07.
+//  Copyright © 2018 Victor Chu. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+    
+
+}
