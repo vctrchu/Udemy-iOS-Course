b3516e6045a09173fd791703afe427dea7f046b4
victorchu8 <victorchu1996@gmail.com>
2018-02-13 11:53:40 -0700

    Initial cocoapod install

---
+++ Breakpoint/Breakpoint/Controller/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Breakpoint
+//
+//  Created by VICTOR CHU on 2018-02-13.
+//  Copyright © 2018 Victor Chu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
